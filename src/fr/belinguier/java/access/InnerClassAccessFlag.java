8f4cafe6b24c8215d81a17085836bf2de18eb395
Eliott Belinguier <eliott.belinguier@epitech.eu>
2021-02-27 21:34:45 +0100

    [add] Access flags enum for Inner Class.

---
+++ src/fr/belinguier/java/access/InnerClassAccessFlag.java
@@ -0,0 +1,41 @@
+package fr.belinguier.java.access;
+
+import java.util.ArrayList;
+
+/**
+ * @author Eliott Belinguier
+ */
+public enum InnerClassAccessFlag {
+
+    PUBLIC((short) 0x0001),
+    PRIVATE((short) 0x0002),
+    PROTECTED((short) 0x0004),
+    STATIC((short) 0x0008),
+    FINAL((short) 0x0010),
+    INTERFACE((short) 0x0200),
+    ABSTRACT((short) 0x0400),
+    SYNTHETIC((short) 0x1000),
+    ANNOTATION((short) 0x2000),
+    ENUM((short) 0x4000);
+
+    private final short value;
+
+    InnerClassAccessFlag(short value) {
+        this.value = value;
+    }
+
+    public short getValue() {
+        return this.value;
+    }
+
+    public static InnerClassAccessFlag[] getFromValue(short value) {
+        ArrayList<InnerClassAccessFlag> result = new ArrayList<InnerClassAccessFlag>();
+
+        for (InnerClassAccessFlag innerClassAccessFlag : InnerClassAccessFlag.values())
+            if ((value & innerClassAccessFlag.getValue()) != 0)
+                result.add(innerClassAccessFlag);
+        if (result.size() < 1)
+            return null;
+        return result.toArray(new InnerClassAccessFlag[result.size()]);
+    }
+}
