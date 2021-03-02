9651f36c060d05b888a2141a7a4087167b45d522
Eliott Belinguier <eliott.belinguier@epitech.eu>
2021-03-02 11:22:31 +0100

    [add] Bootstrap attribute

---
+++ src/fr/belinguier/java/attribute/AttributeBootstrapMethods.java
@@ -0,0 +1,45 @@
+package fr.belinguier.java.attribute;
+
+import fr.belinguier.java.attribute.bootstrap.Method;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AttributeBootstrapMethods extends Attribute {
+
+    private final List<Method> methods;
+
+    public AttributeBootstrapMethods() {
+        this.methods = new ArrayList<Method>();
+    }
+
+    public List<Method> getMethods() {
+        return this.methods;
+    }
+
+    @Override
+    public int sizeOfByteArray() {
+        int methodLength = 0;
+
+        for (Method method : this.methods)
+            methodLength += method.sizeOfByteArray();
+        return super.sizeOfByteArray() + 2  + methodLength;
+    }
+
+    @Override
+    public byte[] toByte() {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(sizeOfByteArray());
+        byte[] temp;
+
+        byteBuffer.putShort(this.nameIndex);
+        byteBuffer.putInt(getLength());
+        byteBuffer.putShort((short) this.methods.size());
+        for (Method method : this.methods) {
+            temp = method.toByte();
+            if (temp != null)
+                byteBuffer.put(temp);
+        }
+        return byteBuffer.array();
+    }
+}
---
+++ src/fr/belinguier/java/attribute/bootstrap/Method.java
@@ -0,0 +1,39 @@
+package fr.belinguier.java.attribute.bootstrap;
+
+import fr.belinguier.java.compiler.Serializable;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Method implements Serializable {
+
+    public short methodRefIndex;
+    private final List<Short> argumentIndexList;
+
+    public Method() {
+        this.argumentIndexList = new ArrayList<Short>();
+    }
+
+    public List<Short> getArgumentIndexList() {
+        return this.argumentIndexList;
+    }
+
+    @Override
+    public int sizeOfByteArray() {
+        int argumentIndexLength = this.argumentIndexList.size() * 2;
+
+        return 2 + argumentIndexLength;
+    }
+
+    @Override
+    public byte[] toByte() {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(sizeOfByteArray());
+
+        byteBuffer.putShort(this.methodRefIndex);
+        byteBuffer.putShort((short) this.argumentIndexList.size());
+        for (Short index : this.argumentIndexList)
+            byteBuffer.putShort(index);
+        return byteBuffer.array();
+    }
+}
