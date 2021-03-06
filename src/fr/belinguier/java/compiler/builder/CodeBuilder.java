ee579338e43d1fe45d6125d24206a03f7dae80e7
Eliott Belinguier <eliott.belinguier@epitech.eu>
2021-03-06 06:11:35 +0100

    [add] Code builder.

---
+++ src/fr/belinguier/java/compiler/builder/CodeBuilder.java
@@ -0,0 +1,57 @@
+package fr.belinguier.java.compiler.builder;
+
+import fr.belinguier.java.compiler.builder.code.ASMInstruction;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author Eliott Belinguier
+ */
+public class CodeBuilder {
+
+    private static int buildSize(Object... instructions) {
+        int result = 0;
+
+        if (instructions == null || instructions.length < 1)
+            return 0;
+        for (Object obj : instructions) {
+            if (obj instanceof ASMInstruction)
+                result += 1;
+            else if (obj instanceof Double || obj instanceof Long)
+                result += 8;
+            else if (obj instanceof Float || obj instanceof Integer)
+                result += 4;
+            else if (obj instanceof Short)
+                result += 2;
+            else if (obj instanceof Byte)
+                result += 1;
+        }
+        return result;
+    }
+
+    public static byte[] build(Object... instructions) {
+        int allocationSize = buildSize(instructions);
+        ByteBuffer byteBuffer;
+
+        if (allocationSize < 1)
+            return null;
+        byteBuffer = ByteBuffer.allocate(allocationSize);
+        for (Object obj : instructions) {
+            if (obj instanceof ASMInstruction)
+                byteBuffer.put(((ASMInstruction) obj).getOpCode());
+            else if (obj instanceof Double)
+                byteBuffer.putDouble((Double) obj);
+            else if (obj instanceof Float)
+                byteBuffer.putFloat((Float) obj);
+            else if (obj instanceof Long)
+                byteBuffer.putLong((Long) obj);
+            else if (obj instanceof Integer)
+                byteBuffer.putInt((Integer) obj);
+            else if (obj instanceof Short)
+                byteBuffer.putShort((Short) obj);
+            else if (obj instanceof Byte)
+                byteBuffer.put((Byte) obj);
+        }
+        return byteBuffer.array();
+    }
+}
---
+++ src/fr/belinguier/java/compiler/builder/code/Instruction.java
@@ -0,0 +1,8 @@
+package fr.belinguier.java.compiler.builder.code;
+
+/**
+ * @author Eliott Belinguier
+ */
+public interface Instruction {
+
+}
