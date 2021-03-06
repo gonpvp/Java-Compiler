b3b6d9f4766ef708d02103019a211b185f061180
Eliott Belinguier <eliott.belinguier@epitech.eu>
2021-03-06 05:56:44 +0100

    [override] equals() and hashCode() methods in ConstantPool to check if there are duplicates of an instance of this class.

--- src/fr/belinguier/java/constant/ConstantPool.java
+++ src/fr/belinguier/java/constant/ConstantPool.java
@@ -2,6 +2,8 @@ package fr.belinguier.java.constant;
 
 import fr.belinguier.java.compiler.Serializable;
 
+import java.util.Objects;
+
 /**
  * @author Eliott Belinguier
  */
@@ -20,5 +22,17 @@ public abstract class ConstantPool implements Serializable {
     @Override
     public int sizeOfByteArray() {
         return 1;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof ConstantPool))
+            return false;
+        return obj.hashCode() == hashCode();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type);
     }
 }
